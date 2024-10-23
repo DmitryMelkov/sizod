0a10eec958c1c53e46b0559242a7a85c391be2c5
Dmitry <melkov.dmitriy40@gmail.com>
2024-10-23 14:41:15 +0500

    add prelouder for report

--- public/js/report.js
+++ public/js/report.js
@@ -1,18 +1,31 @@
 import { dotEkoReportHour } from './dotEkoReportHour.js';
 import { openModal } from './modal.js';
 
-
-
 export const fetchHourlyReport = async () => {
+  const overlay = document.querySelector('.overlay');
+  const modalReport = document.querySelector('.mnemo__modal-report');
+
   try {
+
+    overlay.classList.add('active');
+
+    modalReport.style.visibility = 'hidden';
+
     const response = await fetch(`/api/hourly-report`);
     if (!response.ok) {
       throw new Error('Ошибка сети');
     }
+
     const report = await response.json();
-    dotEkoReportHour(report);
-    openModal('doc-modal');
+
+    setTimeout(() => {
+      overlay.classList.remove('active');
+      modalReport.style.visibility = 'visible';
+      dotEkoReportHour(report);
+      openModal('doc-modal');
+    }, 1000);
   } catch (error) {
+    overlay.classList.remove('active');
     console.error('Ошибка при получении часового отчета:', error);
   }
 };
