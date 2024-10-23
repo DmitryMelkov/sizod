a0d7b94deb7e162c1bb96b25e836de2721af120f
Dmitry <melkov.dmitriy40@gmail.com>
2024-10-23 11:57:28 +0500

    add style report

--- public/js/report.js
+++ public/js/report.js
@@ -1,14 +1,51 @@
 // report.js
 
-import { openModal } from "./modal.js";
+import { openModal } from './modal.js';
 
 export const displayHourlyReport = (report) => {
   const reportDiv = document.getElementById('modal-report-content');
-  reportDiv.innerHTML = '';
+  reportDiv.innerHTML = ''; // Очищаем содержимое
+
+  // Создаем таблицу с классом mnemo__modal-report-table
+  const table = document.createElement('table'); // Объявляем переменную table
+  table.className = 'mnemo__modal-report-table'; // Задаем класс
+  table.style.width = '100%';
+  table.style.borderCollapse = 'collapse';
+  table.style.marginTop = '10px';
+
+  // Добавляем заголовок таблицы
+  const header = table.createTHead();
+  const headerRow = header.insertRow(0);
+  const headers = ['Время', 'Конвейер правый (штук)', 'Конвейер левый (штук)', 'Сумма изделий'];
+
+  headers.forEach((text, index) => {
+    const cell = headerRow.insertCell(index);
+    cell.textContent = text;
+    cell.className = 'mnemo__modal-report-header'; // Задаем класс для заголовка
+  });
+
+  // Добавляем строки с данными
+  const body = table.createTBody();
   for (const hour in report) {
     const entry = report[hour];
-    reportDiv.innerHTML += `<p>${hour}: Правый лыжный: ${entry.rightSki}, Левый лыжный: ${entry.leftSki}, Всего лыж: ${entry.totalSki}</p>`;
+    const row = body.insertRow();
+
+    const time = new Date(hour).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+    const cellTime = row.insertCell(0);
+    cellTime.textContent = time; // Устанавливаем только время
+
+    const cellRight = row.insertCell(1);
+    cellRight.textContent = entry.rightSki;
+
+    const cellLeft = row.insertCell(2);
+    cellLeft.textContent = entry.leftSki;
+
+    const cellTotal = row.insertCell(3);
+    cellTotal.textContent = entry.totalSki;
   }
+
+  // Добавляем таблицу в контейнер
+  reportDiv.appendChild(table);
 };
 
 export const fetchHourlyReport = async () => {
